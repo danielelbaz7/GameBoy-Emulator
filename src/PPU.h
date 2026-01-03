99b0786bace37e848ee06ddd891d34c29790b974
JackyJoh <jackyjoh360@gmail.com>
2026-01-03 00:10:15 -0500

    PPU template
    Co-authored-by: Daniel <danielelbaz12345@gmail.com>

---
+++ src/PPU.h
@@ -0,0 +1,19 @@
+#ifndef GAMEBOYEMULATOR_PPU_H
+#define GAMEBOYEMULATOR_PPU_H
+#include "Gameboy.h"
+
+class PPU {
+
+public:
+    PPU(Gameboy& gb);
+
+
+
+
+private:
+    Gameboy& gb;    
+
+};
+
+
+#endif
