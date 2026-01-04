b42c4ed691a1e49a8645764ff7f8f29d1cedda19
danielelbaz7 <175679070+danielelbaz7@users.noreply.github.com>
2026-01-04 14:10:26 -0500

    implemented sprite buffer loading, dots 0-80. need to improve the code quality but works for now

--- src/PPU.cpp
+++ src/PPU.cpp
@@ -5,6 +5,32 @@ void PPU::UpdatePPU(uint8_t TcyclesSinceLastUpdate) {
     if (TcyclesSinceLastScanline > 456) {
         currentScanline++;
     }
+
+    //if we just hit 80 t cycles and we haven't read OAM yet, now is the time to do that
+    //this takes the first 10 sprites that need to be rendered and places them into the internal sprite buffer
+    if (TcyclesSinceLastScanline > 80 && !modesCompleted[2]) {
+        for (int i = 0; i < totalSprites; i++) {
+            //times 4 since there are 4 bytes per sprite, 40 sprites total
+            uint8_t curSpriteYValue = gb.read(OAMStartAddress + i*bytesPerSprite);
+            if (currentScanline >= (curSpriteYValue - 16) &&
+                currentScanline < (curSpriteYValue - 16 + spriteHeight())) {
+
+                Sprite s{gb.read(OAMStartAddress + i*bytesPerSprite),
+                gb.read(1 +OAMStartAddress + i*bytesPerSprite),
+                gb.read(2 + OAMStartAddress + i*bytesPerSprite),
+                gb.read(3 + OAMStartAddress + i*bytesPerSprite)
+                };
+
+                //increment after using
+                spriteBuffer[nextEmptySlot++] = s;
+
+                if (nextEmptySlot > sizeof(spriteBuffer) * bytesPerSprite) {
+                    break;
+                }
+
+            }
+        }
+    }
 }
 
 void PPU::BeginNewFrame() {
--- src/PPU.h
+++ src/PPU.h
@@ -4,7 +4,7 @@
 
 class PPU {
 public:
-    PPU(Gameboy& gb) : gb(gb) {};
+    explicit PPU(Gameboy& gb) : gb(gb)  {};
     void UpdatePPU(uint8_t TcyclesSinceLastUpdate);
 
 private:
@@ -15,10 +15,29 @@ private:
     bool modesCompleted[4]{}; //mode 2 is OAM, then mode 3 is drawing pixels,
     //then mode 0 is horizontal blank, and mode 1 is vertical blank
 
+    //lambda function that returns the current sprite height at any point
+    [[nodiscard]] uint8_t spriteHeight() const {
+        return gb.read(0xFF40) & 0x04;
+    }
+
+    constexpr static uint16_t OAMStartAddress = 0xFE00;
+    constexpr static uint8_t totalSprites = 40;
+    constexpr static uint8_t bytesPerSprite = 4;
+
     //resets all values to that of a new frame
     void BeginNewFrame();
 
+    struct Sprite {
+        uint8_t x;
+        uint8_t y;
+        uint8_t tile;
+        uint8_t flags;
+    };
+
+    Sprite spriteBuffer[10] = {};
+    uint8_t nextEmptySlot{};
 };
+
 
 
 #endif
